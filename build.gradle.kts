a1256c063cabd12499394d9d995053377d70b839
SsangSoo <kss10080@naver.com>
2026-01-23 23:22:49 +0900

    QueryDSL 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,6 +61,12 @@ dependencies {
     // redisson
     implementation("org.redisson:redisson-spring-boot-starter:3.52.0")
 
+    // QueryDSL
+    implementation ("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    annotationProcessor ("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    annotationProcessor ("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor ("jakarta.persistence:jakarta.persistence-api")
+
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 
